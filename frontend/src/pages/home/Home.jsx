26ad1a94f0d734685ff6e90d1d14536519a25733
AaseemNadaf <aaseem.nadaf@gmail.com>
2025-04-23 03:03:10 +0530

    Final Project

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,18 @@
-import Banner from "./Banner"
+import React from 'react'
+import Banner from './Banner'
+import TopSellers from './TopSellers'
+import Recommened from './Recommened'
+import News from './News'
 
 const Home = () => {
-    return (
-        <>
-            <Banner/>
-        </>
-    )
+  return (
+    <>
+        <Banner/>
+        <TopSellers/>
+        <Recommened/>
+        <News/>
+    </>
+  )
 }
 
 export default Home
